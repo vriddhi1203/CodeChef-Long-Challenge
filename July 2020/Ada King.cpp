8d6b799eb7621e352bb715854ab11b218eb099e7
sanchi0204 <sanchirastogi02@gmail.com>
2020-07-13 01:56:36 +0530

    Solutions to questions solved in July Challenge

---
+++ July 2020/Ada King.cpp
@@ -0,0 +1,39 @@
+//Codechef July Long Challenge
+//Ada King
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+
+	while(t>0)
+	{
+	    int n;
+	    cin>>n;
+	    int count=1;
+
+	    for(int i=0;i<8;i++)
+	    {
+	        for(int j=0;j<8;j++)
+	        {
+	            if(count<n)
+	             cout<<".";
+
+	            else if(count==n)
+	             cout<<"O";
+
+	            else
+	                cout<<"X";
+
+	        count+=1;
+	        }
+
+	        cout<<endl;
+	    }
+	    t--;
+	}
+	return 0;
+}
---
+++ July 2020/Missing a Point.cpp
@@ -0,0 +1,33 @@
+//Codechef July Long Challenge
+// Missing a Point
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+
+	while(t>0)
+	{
+	    long long int n;
+	    cin>>n;
+	int xor_x=0, xor_y=0;
+
+	for(int i=0;i<4*n-1;i++)
+	{
+	   long long int x,y;
+	   cin>>x>>y;
+
+	   xor_x^=x;
+	   xor_y^=y;
+
+	}
+
+	cout<<xor_x<<" "<<xor_y<<endl;
+
+	    t--;
+	}
+	return 0;
+}
